88d900ff06607b611bf011b167b21d097e514929
Hongsuk Choi <31090011+hongsukchoi@users.noreply.github.com>
2020-09-03 14:48:23 +0900

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,5 @@
 pip install opencv-python
-pip intsall transforms3d
+pip install transforms3d
 pip install pycocotools
 pip install git+https://github.com/scottandrews/chumpy.git@fe51783e0364bf1e9b705541e7d77f894dd2b1ac
 pip install pyrender
