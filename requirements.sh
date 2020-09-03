8c96f82a33209e2f0ddab67873457bd832e21b06
Hongsuk Choi <31090011+hongsukchoi@users.noreply.github.com>
2020-09-03 14:51:34 +0900

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -4,6 +4,6 @@ pip install pycocotools
 pip install git+https://github.com/scottandrews/chumpy.git@fe51783e0364bf1e9b705541e7d77f894dd2b1ac
 pip install pyrender
 pip install trimesh
-pip install yaml
+pip install pyyaml
 pip install easydict
 pip install tqdm
